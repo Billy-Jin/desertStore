f25219ea66dfe72c1a2e2527df2e9ca53e1bfd30
breadmk <minvita9999@naver.com>
2022-01-27 21:11:51 +0900

    SpringBoot setting

---
+++ src/main/java/kr/co/dessertStore/DessertStoreApplication.java
@@ -0,0 +1,13 @@
+package kr.co.dessertStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessertStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessertStoreApplication.class, args);
+	}
+
+}
