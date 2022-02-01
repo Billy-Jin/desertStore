f12b94347e96b9edb89922c230622015e76bbec1
breadmk <minvita9999@naver.com>
2022-02-01 15:03:07 +0900

    bootstrap&router(5.2.0)&stled-component 추가 및 로그인View

--- dessert/src/App.js
+++ dessert/src/App.js
@@ -2,12 +2,17 @@ import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import "./App.css";
+import LoginForm from "./components/login/LoginForm";
+import { Route } from "react-router-dom";
+import JoinForm from "./components/login/JoinForm";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      <Main />
+      <Route exact path="/loginForm" component={LoginForm} />
+      <Route exact path="/joinForm" component={JoinForm} />
+      <Route exact path="/" component={Main} />
       <Footer />
     </div>
   );
